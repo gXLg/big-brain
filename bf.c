4ac4db600289bf4d5680c1cd1d502dd260560e26
Kemuri Senpai <65429873+gXLg@users.noreply.github.com>
2021-04-19 12:46:32 +0200

    Create bf.c

---
+++ bf.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+const size_t f = 65536;
+
+unsigned char game [ 2048 ] = { 0 };
+unsigned char *ptr = game;
+char file [ f ];
+
+int main ( int argc, char **argv ) {
+  FILE *prog;
+  if ( ! ( prog = fopen ( argv [ 1 ], "r" ))) {
+    fprintf ( stderr, "can't open the file \"%s\".\n", argv [ 1 ]);
+    return 2;
+  }
+  fread ( file, f, 1, prog );
+  fclose ( prog );
+
+  char current_char;
+  size_t i;
+  size_t loop;
+
+  for ( i = 0; file [ i ] != 0; i ++ ) {
+    current_char = file [ i ];
+    if ( current_char == '>' ) ++ ptr;
+    else if ( current_char == '<' ) -- ptr;
+    else if ( current_char == '+' ) ++ *ptr;
+    else if ( current_char == '-' ) -- *ptr;
+    else if ( current_char == '.' ) putchar ( *ptr );
+    else if ( current_char == ',' ) *ptr = getchar ( );
+    else if ( current_char == '[' ) continue;
+    else if ( current_char == ']' && *ptr ) {
+      loop = 1;
+      while ( loop > 0 ) {
+        current_char = file [ -- i ];
+        if ( current_char == '[' ) loop --;
+        else if ( current_char == ']' ) loop ++;
+      }
+    }
+  }
+}
